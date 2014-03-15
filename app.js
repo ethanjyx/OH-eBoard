e62c011d5e36e737c598d38e3ba96ed0ab8df3d0
tjnkys <yshuo@umich.edu>
2014-03-15 19:47:05 -0400

    change to oreilly

--- app.js
+++ app.js
@@ -1,85 +1,128 @@
-/*
-    This file is generated and updated by Sencha Cmd. You can edit this file as
-    needed for your application, but these edits will have to be merged by
-    Sencha Cmd when it performs code generation tasks such as generating new
-    models, controllers or views and when running "sencha app upgrade".
+//<debug>
+Ext.Loader.setPath({
+    'Ext': 'touch/src',
+    'testapp': 'app'
+});
+//</debug>
 
-    Ideally changes to this file would be limited and most work would be done
-    in other places (such as Controllers). If Sencha Cmd cannot merge your
-    changes and its generated code, it will produce a "merge conflict" that you
-    will need to resolve manually.
-*/
+Ext.require('testapp.util.Proxy');
 
 Ext.application({
-    name: 'OHeBoard',
+    // Change the values below to re-configure the app for a different conference.
 
-    requires: [
-        'Ext.MessageBox'
+    title:   'Web 2.0 Summit 2010',
+    dataUrl: 'http://en.oreilly.com/web2010/public/mobile_app/all',
+
+    twitterSearch: '#w2s',
+
+    mapCenter: [37.788539, -122.401643],
+    mapText: 'The Palace Hotel<br /><small>2 New Montgomery Street<br />San Francisco, CA 94105<br />(415) 512-1111</small>',
+
+    aboutPages: [
+        {
+            title: 'Overview',
+            xtype: 'htmlPage',
+            url: 'data/about.html'
+        },
+        {
+            title: 'Sponsors',
+            xtype: 'htmlPage',
+            url: 'data/sponsors.html'
+        },
+        {
+            title: 'Credits',
+            xtype: 'htmlPage',
+            url: 'data/credits.html'
+        },
+        {
+            title: 'Videos',
+            xtype: 'videoList',
+            playlistId: '2737D508F656CCF8',
+            hideText: 'Web 2.0 Summit 2010: '
+        }
+    ],
+
+    // App namespace
+
+    name: 'testapp',
+
+    startupImage: {
+        '320x460': 'resources/startup/Default.jpg', // Non-retina iPhone, iPod touch, and all Android devices
+        '640x920': 'resources/startup/640x920.png', // Retina iPhone and iPod touch
+        '640x1096': 'resources/startup/640x1096.png', // iPhone 5 and iPod touch (fifth generation)
+        '768x1004': 'resources/startup/768x1004.png', //  Non-retina iPad (first and second generation) in portrait orientation
+        '748x1024': 'resources/startup/748x1024.png', //  Non-retina iPad (first and second generation) in landscape orientation
+        '1536x2008': 'resources/startup/1536x2008.png', // : Retina iPad (third generation) in portrait orientation
+        '1496x2048': 'resources/startup/1496x2048.png' // : Retina iPad (third generation) in landscape orientation
+    },
+
+    isIconPrecomposed: false,
+    icon: {
+        57: 'resources/icons/icon.png',
+        72: 'resources/icons/icon@72.png',
+        114: 'resources/icons/icon@2x.png',
+        144: 'resources/icons/icon@72.png'
+    },
+
+    // Dependencies
+
+    requires: ['testapp.util.Proxy'],
+
+    models: [
+        'Session',
+        'Speaker'
     ],
 
     views: [
-        'Main'
+        'Main',
+
+        'session.Card',
+        'session.List',
+        'session.Detail',
+        'session.Info',
+
+        'speaker.Card',
+        'speaker.List',
+        'speaker.Detail',
+        'speaker.Info',
+
+        'Tweets',
+        'Location',
+
+        'about.Card',
+        'about.List',
+        'about.HtmlPage',
+        'about.VideoList'
     ],
 
-    icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
-    },
+    controllers: [
+        'Sessions',
+        'Speakers',
+        'Tweets',
+        'About'
+    ],
 
-    isIconPrecomposed: true,
-
-    startupImage: {
-        '320x460': 'resources/startup/320x460.jpg',
-        '640x920': 'resources/startup/640x920.png',
-        '768x1004': 'resources/startup/768x1004.png',
-        '748x1024': 'resources/startup/748x1024.png',
-        '1536x2008': 'resources/startup/1536x2008.png',
-        '1496x2048': 'resources/startup/1496x2048.png'
-    },
-
-    loadScript: function(url, callback) {
-        // Adding the script tag to the head as suggested before
-        
-    },
+    stores: [
+        'Sessions',
+        'SpeakerSessions',
+        'Speakers',
+        'SessionSpeakers',
+        'Tweets',
+        'Videos'
+    ],
 
     launch: function() {
-        // callback function
-        var applicationInit = function() {
-            // alert('parse?')
-            Parse.initialize("Wc5ZhPmum7iezzBsnuYkC9h2yQdrPseP4mzpyUPv", "D0RPSRjcEos1Y8RdblqmuERytSRM5z5xdw9dFUT5");
-            
-            // Destroy the #appLoadingIndicator element
-            Ext.fly('appLoadingIndicator').destroy();
-            // Initialize the main view
-            Ext.Viewport.add(Ext.create('OHeBoard.view.Main'));
-        };
 
-        // load Parse JS SDK
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = "http://www.parsecdn.com/js/parse-1.2.13.min.js";
+        Ext.Viewport.setMasked({ xtype: 'loadmask' });
 
-        // Then bind the event to the callback function.
-        // There are several events for cross browser compatibility.
-        script.onreadystatechange = applicationInit;
-        script.onload = applicationInit;
+        testapp.util.Proxy.process('data/feed.js', function() {
+            Ext.Viewport.add({ xtype: 'main' });
+            Ext.Viewport.setMasked(false);
+        });
 
-        // Fire the loading
-        head.appendChild(script);
-    },
-
-    onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
-                }
-            }
-        );
+        // setInterval(function(){
+        //     Ext.DomQuery.select('link')[0].href = "resources/css/oreilly.css?" + Math.ceil(Math.random() * 100000000)
+        // }, 1000);
     }
+
 });
