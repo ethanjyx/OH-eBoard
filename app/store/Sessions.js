84bb568ca1448a1ac5d50af06be9745a2c3a3e63
Tingyi <tingyi@Tingyis-MacBook-Pro.local>
2014-03-29 21:45:04 -0400

    load me on tab tap

--- app/store/Sessions.js
+++ app/store/Sessions.js
@@ -21,21 +21,6 @@ Ext.define('testapp.store.Sessions', {
                 property: 'courseNumber',
                 direction: 'ASC'
             }
-        ]/*,
-
-        proxy: {
-            type: 'rest',
-            useDefaultXhrHeader: false,
-            url: 'https://api.parse.com/1/classes/JoinTable',
-            headers: {
-                'X-Parse-Application-Id' : "Wc5ZhPmum7iezzBsnuYkC9h2yQdrPseP4mzpyUPv", 
-                'X-Parse-REST-API-Key' : "6FgZ9ItKztfQOmQmtmZzvOdaVDSSNhOeZfuG2N1g",
-                'Content-Type' : 'application/json'
-            },
-            reader: {
-                type: 'json',
-                rootProperty: 'results'
-            }
-        }*/
+        ]
     }
 });
